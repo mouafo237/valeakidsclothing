cbd3e2884a4db6b309f7738a97e2764a96142b8c
app <mouafotene001@gmail.com>
2025-03-18 15:02:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -447,24 +447,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 colors: ["Rouge", "Bleu", "Vert"],
                 sizes: ["2-3 ans", "4-5 ans", "6-7 ans", "8-9 ans"]
             },
-             {
-                name: "Jupe Plissée",
-                price: "$26.99",
-                image: "img/r.png",
-                category: "vetements",
-                rating: 4,
-                colors: ["Rouge", "Bleu", "Vert"],
-                sizes: ["2-3 ans", "4-5 ans", "6-7 ans", "8-9 ans"]
-            },
-            {
-                name: "Jupe Plissée",
-                price: "$26.99",
-                image: "img/r.png",
-                category: "vetements",
-                rating: 4,
-                colors: ["Rouge", "Bleu", "Vert"],
-                sizes: ["2-3 ans", "4-5 ans", "6-7 ans", "8-9 ans"]
-            },
             {
                 name: "Jupe Plissée",
                 price: "$26.99",
